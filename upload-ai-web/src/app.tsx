e992289beb63677517f82a248779755966fa10c6
thbueno <thiagobueno.s@gmail.com>
2023-10-10 10:36:30 -0300

    segundo commit

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -37,7 +37,7 @@ export function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
+        <h1 className="text-xl font-bold">YouTube Content Creator .ai</h1>
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
