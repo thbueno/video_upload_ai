d562ea5df5d97481d671d48c0ccb6425855ab4ce
thbueno <thiagobueno.s@gmail.com>
2023-10-10 16:28:06 -0300

    Alteração no desenvolido por

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -41,7 +41,7 @@ export function App() {
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜 no NLW da Rocketseat
+            Desenvolvido com 💚 e ☕ por Thiago Bueno
           </span>
 
           <Separator orientation="vertical" className="h-6" />
